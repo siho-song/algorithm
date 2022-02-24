91b96dd846e884b517f758be434ce15aa9cbf892
sshow <79968994+siho-song@users.noreply.github.com>
2022-02-25 01:54:24 +0900

    Delete 10430.py

--- 10430.py
+++
@@ -1,6 +0,0 @@
-numbers=list(map(int,(input().split())))
-
-print((numbers[0]+numbers[1])%numbers[2])
-print(((numbers[0]%numbers[2])+(numbers[1]%numbers[2]))%numbers[2])
-print((numbers[0]*numbers[1])%numbers[2])
-print(((numbers[0]%numbers[2]) * (numbers[1]%numbers[2]))%numbers[2])
